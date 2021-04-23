3392d6d54c001f3aa851381cd6ed377ec08df750
elysiaestiningtyas <elsa02018@gmail.com>
2021-04-23 14:09:09 +0700

    Menambahkan fitur login ref #4

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.auth')
+
+@section('main-content')
+<div class="container">
+    <div class="row">
+        <div class="col-6 col-md-4"></div>
+        <div class="col-6 col-md-4"> <br><br><br>
+                
+                <div class="card-body">
+                                <div class="text-center">
+                                    <h1 class="h3 text-gray-900 mb-4"><b>{{ __('Masuk') }}</b></h1>
+                                </div>
+<hr width="25%" style="border: solid #F57ABC ">
+                                @if ($errors->any())
+                                    <div class="alert alert-danger border-left-danger" role="alert">
+                                        <ul class="pl-4 my-2">
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+
+                                <form method="POST" action="{{ route('login') }}" class="user">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                                    <div class="form-group">
+                                        <input type="email" class="form-control col" name="email" placeholder="{{ __('Alamat Email / Username') }}" value="{{ old('email') }}" required autofocus>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <input type="password" class="form-control col" name="password" placeholder="{{ __('Masukkan Passwordmu') }}" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox small">
+                                            <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                            <label class="custom-control-label" for="remember">{{ __('Ingat Saya') }}</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <button type="submit" class="btn col" style="background-color: #F57ABC; color: white; box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);">
+                                            {{ __('Masuk') }}
+                                        </button>
+                                    </div>
+
+                                    {{-- <hr> --}} <br>
+
+                                    <!-- <div class="form-group">
+                                        <button type="button" class="btn btn-github btn-user btn-block">
+                                            <i class="fab fa-github fa-fw"></i> {{ __('Login with GitHub') }}
+                                        </button>
+                                    </div>
+
+                                    
+
+                                    <div class="form-group">
+                                        <button type="button" class="btn btn-facebook btn-user btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> {{ __('Login with Facebook') }}
+                                        </button>
+                                    </div> -->
+                                </form>
+
+                                {{-- <hr> --}}
+
+                                @if (Route::has('password.request'))
+                                    <div class="text-center">
+                                        <a class="small" style="color: black;" href="{{ route('password.request') }}">
+                                            {{ __('Lupa Password?') }}
+                                        </a>
+                                    </div>
+                                @endif
+
+                                @if (Route::has('register'))
+                                    <div class="text-center">
+                                        
+                                        <a class="small" style="color: #FF3BA4;" href="{{ route('register') }}">{{ __('Belum Punya Akun? Daftar Sekarang!') }}</a>
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
