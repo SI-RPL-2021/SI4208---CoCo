b432a960b4e840f3af68c734dbb8dbb738357a65
GINNA TRIGAMA <ginnatrigama@gmail.com>
2021-06-03 20:15:41 +0700

    perbaikan fitur chat

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,140 +1,90 @@
 @extends('layouts.navbar')
 
 @section('main-content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <title>Document</title>
+</head>
 
-<div class="container-chat">
-    <div class="row">
-        <div class="col-3" style="float:left; background: #FFEAFF;">
-            <center>
-                <img src="img/ariana.jpg" style="width: 50px; height: 50px; border-radius:50%;">
-                <h5>Ariana Grande</h5>
-                <p>@arianagrande</p>
-                <p>227 m followers | 750 following</p>
-            </center>
 
-            <br><br>
-            <p style="text-align:left"><b>Kontak</b></p>
-            <table class="table table-borderless">
-                <tbody>
+<body>
+    <div class="container-kiri" style="float:left; width:25%; margin-top:50px">
+        <center>
+            <img src="img/ariana.jpg" style="width: 50px; height: 50px; border-radius:50%;">
+            <h5>Ariana Grande</h5>
+            <p>@arianagrande</p>
+            <p>227 m followers | 750 following</p>
+        </center>
+        <img src="img/justin.jpg" style="width: 50px; height: 50px; border-radius:50%; float:left;">
+        <label style="text-indent: 2em;"><b>Justin Bieber</b>
+            <p>@justinbie</p>
+        </label>
+        <br>
+        <img src="img/billie.jpg" style="width: 50px; height: 50px; border-radius:50%;">
+        <label style="text-indent: 2em;"><b>Billie Eilish</b>
+            <p>@billiecans</p>
+        </label>
+        <br>
+        <img src="img/sean.jpg" style="width: 50px; height: 50px; border-radius:50%;">
+        <label style="text-indent: 2em;"><b>Sean</b>
+            <p>@seanuhuy</p>
+        </label>
+        <br>
+        <img src="img/keanu.jpg" style="width: 50px; height: 50px; border-radius:50%;">
+        <label style="text-indent: 2em;"><b>Keanu Angel</b>
+            <p>@keanununu</p>
+        </label>
 
-                    <tr>
-                        <th width="20px"></th>
-                        <th width="20px"></th>
-                    </tr>
+    </div>
+        
+    <div class="container-tengah" style="float:left; width:50%;margin-top:50px">
+        <center>
+            <img src="img/chat.png" style="width: 250px; height: 200px" alt="">
+            <h3 style="font-weight: bold; color: #EF2A82;">Tidak Ada Pesan Disini</h3>
 
-                    <tr>
-                        <th scope="row">
-                        <td><img src="img/justin.jpg" style="width: 50px; height: 50px; border-radius:50%; float:left;"></td>
-                        <td><label style=" float:left; text-indent: 2em;"><b>Justin Bieber</b>
-                                <p>@justinbie</p>
-                            </label> </td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row">
-                        <td><img src="img/billie.jpg" style="width: 50px; height: 50px; border-radius:50%; float:left;"></td>
-                        <td><label style=" float:left; text-indent: 2em;"><b>Billie Eilish</b>
-                                <p>@billiecans</p>
-                            </label> </td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row">
-                        <td><img src="img/sean.jpg" style="width: 50px; height: 50px; border-radius:50%; float:left;"></td>
-                        <td><label style=" float:left; text-indent: 2em;"><b>Sean</b>
-                                <p>@seanuhuy</p>
-                            </label> </td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row">
-                        <td><img src="img/keanu.jpg" style="width: 50px; height: 50px; border-radius:50%; float:left;"></td>
-                        <td><label style=" float:left; text-indent: 2em;"><b>Keanu Angel</b>
-                                <p>@keanununu</p>
-                            </label> </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-
-        <div class="col-8" style="background: #FCF2FF;float:left;">
-            <center>
-                <img src="img/chat.png" style="width: 250px; height: 200px" alt="">
-                <h3 style="font-weight: bold; color: #EF2A82; margin-top: 40px">Tidak Ada Pesan Disini</h3>
-            </center>
-            <br><br>
-            <table class="table table-borderless">
-                <tbody>
-                    <tr>
-                        <th scope="row">
-                        <td>
-                            <div class="form-floating">
-                                <textarea class="form-control" placeholder="kirim pesan..." id="floatingTextarea2" style="height: 45px; float:left;"></textarea>
-                                <label for="floatingTextarea2"></label>
-                        </td>
-                        <td><button type="submit" style="background-color: #EF2A82; color: white; float:left;" name="kirim">Kirim</button></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+            <div class="form-floating">
+                <textarea class="form-control" placeholder="kirim pesan..." id="floatingTextarea2" style="height: 45px; float:left;width: 90%;"></textarea>
+                
+                <button type="submit" style="background-color: #EF2A82; color: white; float:left; margin-left:10px" name="kirim">Kirim</button>
+            </div>
+        </center>
     </div>
 
-    <div class="col-3" style="background: #FFEAFF; float:left;">
-        <div class="form-floating">
-            <textarea class="form-control" placeholder="cari teman" id="floatingTextarea2" style="height: 45px; float:left; background-color:black; color:white;"></textarea>
-            <label for="floatingTextarea2"></label> <br><br><br>
-
-            <table class="table table-borderless">
-                <tbody>
-
-                    <tr>
-                        <th width="20px"></th>
-                        <th width="20px"></th>
-                    </tr>
-
-                    <tr>
-                        <th scope="row">
-                        <td><img src="img/joey.jpg" style="width: 50px; height: 50px; border-radius:50%; float:left;"></td>
-                        <td><label style=" float:left; text-indent: 2em;"><b>Joey King</b>
-                                <p>@joeyjoy</p>
-                            </label> </td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row">
-                        <td><img src="img/noah.jpg" style="width: 50px; height: 50px; border-radius:50%; float:left;"></td>
-                        <td><label style=" float:left; text-indent: 2em;"><b>Noah Centi</b>
-                                <p>@noahahaha</p>
-                            </label> </td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row">
-                        <td><img src="img/taylor.jpg" style="width: 50px; height: 50px; border-radius:50%; float:left;"></td>
-                        <td><label style=" float:left; text-indent: 2em;"><b>Taylor Swift</b>
-                                <p>@taylorskuy</p>
-                            </label> </td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row">
-                        <td><img src="img/ross.jpg" style="width: 50px; height: 50px; border-radius:50%; float:left;"></td>
-                        <td><label style=" float:left; text-indent: 2em;"><b>Ross Butler</b>
-                                <p>@rossbutt</p>
-                            </label> </td>
-                    </tr>
-
-                    <tr>
-                        <th scope="row">
-                        <td><img src="img/lana.jpg" style="width: 50px; height: 50px; border-radius:50%; float:left;"></td>
-                        <td><label style=" float:left; text-indent: 2em;"><b>Lana</b>
-                                <p>@lananana</p>
-                            </label> </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+    <div class="container-kanan" style="float:left; width:25%;margin-top:50px">
+        <img src="img/joey.jpg" style="width: 50px; height: 50px; border-radius:50%; float:left;">
+        <label style=" float:left; text-indent: 2em;"><b>Joey King</b>
+            <p>@joeyjoy</p>
+        </label>
+        <br>
+        <img src="img/noah.jpg" style="width: 50px; height: 50px; border-radius:50%;">
+        <label style="text-indent: 2em;"><b>Noah</b>
+            <p>@noahahaha</p>
+        </label>
+        <br>
+        <img src="img/taylor.jpg" style="width: 50px; height: 50px; border-radius:50%;">
+        <label style="text-indent: 2em;"><b>Taylor Swift</b>
+            <p>@taylorskuy</p>
+        </label>
+        <br>
+        <img src="img/ross.jpg" style="width: 50px; height: 50px; border-radius:50%;">
+        <label style="text-indent: 2em;"><b>Ross Butler</b>
+            <p>@rossbut</p>
+        </label>
+        <br>
+        <img src="img/lana.jpg" style="width: 50px; height: 50px; border-radius:50%;">
+        <label style="text-indent: 2em;"><b>Lana</b>
+            <p>@lananana</p>
+        </label>
     </div>
-</div>
-</div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+</body>
 @endsection
+</html>
