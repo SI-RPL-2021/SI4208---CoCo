af1bfc443e8f2e539e5da906f4b681c925719672
melshnz <shahnaz2kamilah@gmail.com>
2021-06-18 10:36:31 +0700

    memperbaiki fitur ubah profile milik pengguna

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Users;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Images;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
@@ -17,21 +18,11 @@ class ProfileController extends Controller
     }
 
     public function index()
-    {
+    {   
         $user = Auth::user();
-        $users = $this->getAllPost();
-        $name = $user['name'];
-        $username = $user['username'];
-        $email = $user['email'];
-        $bio = $user['bio'];
-        $image = $user['images_id'];
         return view('profile', [
-            'name' => $name,
-            'username' => $username,
-            'email' => $email,
-            'bio' => $bio,
-            'image' => $image,
-            'users' => $users
+            'user'=>$user,
+            'alert'=>''
         ]);
     }
 
@@ -44,39 +35,6 @@ class ProfileController extends Controller
         return Storage::get('public/' . $path);
     }
 
-    /**
-     * Update user's profile
-     *
-     * @param  UpdateProfileRequest $request
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-
-    // public function updateProfile(Request $request)
-    // {
-    //     $user = Auth::user();
-
-    //     $request->user()->updateProfile(
-    //         $request->all()
-    //     );
-
-    // $user = new Users;
-    // $user->name = $request['name'];
-    // $user->username = $request['username'];
-    // $user->bio = $request['bio'];
-    // $user->users_id = $user['id'];
-    // $user->save();
-
-    // return redirect('profile');
-
-    // $request->validate([
-    //     'nama' => 'required',
-    //     'username' => 'required',
-    //     'bio' => 'required'
-    // ]);
-    // $users->update($request->all());
-    // return redirect()->route('biodata.index')->with('success','biodata berhasil di update');
-    // }
-
     public function update()
     {
         $users = DB::select('select * from users');
@@ -87,18 +45,28 @@ class ProfileController extends Controller
         $users = DB::select('select * from users where id = ?', [$id]);
         return view('profile', ['users' => $users]);
     }
-    public function edit(Request $request, $id)
+    public function updateProfile(Request $request)
     {
         $name = $request->input('name');
         $username = $request->input('username');
         $bio = $request->input('bio');
-        /*$data=array('first_name'=>$first_name,"last_name"=>$last_name,"city_name"=>$city_name,"email"=>$email);*/
-        /*DB::table('student')->update($data);*/
-        /* DB::table('student')->whereIn('id', $id)->update($request->all());*/
-        DB::update('update users set name = ?,username=?,bio=? where id = ?', [$name, $username, $bio, $id]);
-        echo "Record updated successfully.
-        ";
-        echo 'Click Here to go back.';
+        $id = Auth::user()->id;
+        $query = "update users set name='".$name."', username='".$username."', bio='".$bio."'";
+        if($request->newImage) {
+            $newImage = $request->newImage;
+            $imageName = time().''.$newImage->getClientOriginalName();
+            $newImage->storeAs('public/profile-images/', $imageName);
+            $image = new Images();
+            $image->url = 'profile-images/'.$imageName;
+            $image->save();
+            $query .= ", images_id='".$image->id."'";
+        }
+        DB::update($query.' where id = ?', [$id]);
+        $user = Users::find(Auth::user()->id);
+        return view('profile', [
+            'user'=>$user,
+            'alert'=>'Berhasil Update Data Profil.'
+        ]);
     }
 
     public function getAllPost()
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,43 +2,56 @@
 
 @section('main-content')
 
+@if($alert!='')
+    <div class="alert alert-primary" role="alert">
+        {{$alert}}
+    </div>
+@endif
+
 <div class="container-sm" style="background-color: #FFEAFF;">
     <br>
     <h3 style="margin-left: 50px;"><b>Profile</b></h3>
     <br>
 
     <div class="container-2" style="background-color: #FFFFFF;">
-        <!-- INSERT PHOTO HERE -->
-        <div class="container-3" style="background-color: #FFFFFF;">
-            <div class="col-auto">
+        <form method="POST" action="{{ route('updateProfile') }}" enctype="multipart/form-data">
+            
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <!-- INSERT PHOTO HERE -->
+            <div class="container-3" style="background-color: #FFFFFF;">
+                <div class="col-auto">
                     <!-- INSERT PHOTO -->
                     <div class="image-event">
                         <a href="">
-                            <img src="{{ url('displayImage/'.$image.'')}}" style="width: 120px; height: 120px;" alt="">
+                            <img src="{{ url('displayImage/'.$user->images_id.'')}}" style="width: 100px; height: 100px; margin:2px" alt="">
                         </a>
                         <br>
                     </div>
+                    <!-- UPLOAD FOTO -->
+                    <label for="formFileSm" class="form-label"><b>Upload foto baru</b></label>
+                    <input name="newImage" class="form-control" id="formFileSm" type="file"/>
                     <!-- NAMA USER -->
                     <label for="nama_user" class="form-label"><b>Nama</b></label>
-                    <input name="name" type="text" id="name" class="form-control" aria-describedby="" value="<?php echo$users[0]->name; ?>">
+                    <input name="name" type="text" id="name" class="form-control" aria-describedby="" value="<?php echo $user->name; ?>" required>
                     <br>
                     <!-- USERNAME -->
                     <label for="nama_username" class="form-label"><b>Username</b></label>
-                    <input name="username" type="text" id="inputUsername" class="form-control" aria-describedby="" value="<?php echo$users[0]->username; ?>">
+                    <input name="username" type="text" id="inputUsername" class="form-control" aria-describedby="" value="<?php echo $user->username; ?>" required>
                     <br>
                     <!-- EMAIL -->
                     <label for="nama_email" class="form-label"><b>E-mail</b></label>
-                    <input name="email" type="text" readonly class="form-control-plaintext" id="staticEmail" class="form-control disabled" aria-describedby="" value="{{$email}}">
+                    <input name="email" type="text" readonly class="form-control-plaintext" id="staticEmail" class="form-control disabled" aria-describedby="" value="{{$user->email}}">
                     <br>
                     <!-- BIO -->
                     <label for="inputJudul" class="form-label"><b>Bio</b></label>
-                    <textarea name="bio" id="inputDeskripsi" class="form-control" rows="5" value="<?php echo$users[0]->bio; ?>"></textarea>
+                    <textarea name="bio" id="inputDeskripsi" class="form-control" rows="5"><?php echo $user->bio; ?></textarea>
                     <br>
                     <button type="submit" class="btn" style="float: right; background-color:#F57ABC; color:white;"><b>Simpan</b></button>
                     <br>
-            </div>
-        </div>
+                </div>
+        </form>
     </div>
+</div>
 
 </div>
 
