974eeadae31a359cd821df5e9870c0ddd075bfd1
melshnz <shahnaz2kamilah@gmail.com>
2021-06-18 10:33:43 +0700

    Menambahkan fitur melihat profile teman ref #28

---
+++ app/Http/Controllers/ProfileViewController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Posts;
+use App\Models\Events;
+use App\Models\Follows;
+use App\Models\Users;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
+
+class ProfileViewController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function redirectProfileView($id){
+        return Redirect::to("/profileView/diskusi/".$id);
+    }
+
+    public function profileView($id) {
+        $user = Users::where('id','=',$id)->first();
+        $id = $user['id'];
+        $name = $user['name'];
+        $username = $user['username'];
+        $image = $user['images_id'];
+        $posts = $this->getPostById($id);
+
+        $following = $user->following()->count();
+        $follower = $user->followers()->count();
+
+        $userId = Auth::user()->id;
+        $is_following = Follows::where('follower_id', '=', $userId)
+                                ->where('following_id', '=', $id)->first() ||
+                        $id == Auth::user()->id? true:false;
+        return view('profileView', [
+            'name' => $name,
+            'username' => $username,
+            'image' => $image,
+            'posts' => $posts,
+            'id' => $user->id,
+            'following' => $following,
+            'follower' => $follower,
+            'isFollowing' => $is_following,
+            'isDiskusi' => true
+        ]);
+    }
+
+    public function profileViewEvent($id) {
+        $user = Users::where('id','=',$id)->first();
+        $id = $user['id'];
+        $name = $user['name'];
+        $username = $user['username'];
+        $image = $user['images_id'];
+        $events = $this->getEventsById($id);
+
+        $following = $user->following()->count();
+        $follower = $user->followers()->count();
+
+        $userId = Auth::user()->id;
+        $is_following = Follows::where('follower_id', '=', $userId)
+                                ->where('following_id', '=', $id)->first() ||
+                        $id == Auth::user()->id? true:false;
+        return view('profileView', [
+            'name' => $name,
+            'username' => $username,
+            'image' => $image,
+            'events' => $events,
+            'id' => $user->id,
+            'following' => $following,
+            'follower' => $follower,
+            'isFollowing' => $is_following,
+            'isDiskusi' => false
+        ]);
+    }
+
+    public function displayImage($id)
+    {
+        $image = DB::table('images')
+            ->where('id', '=', $id)
+            ->get();
+        $path = $image[0]->url;
+        return Storage::get('public/' . $path);
+    }
+
+    public function getPostById($id)
+    {
+        $posts = Posts::where('users_id', '=', $id)
+                        ->orderBy('updated_at', 'DESC')->get();
+
+        return $posts;
+    }
+
+    public function getEventsById($id)
+    {
+        $events = Events::where('users_id', '=', $id)
+                        ->orderBy('updated_at', 'DESC')->get();
+        return $events;
+    }
+}
---
+++ resources/views/profileView.blade.php
@@ -0,0 +1,141 @@
+@extends('layouts.navbar')
+
+@section('main-content')
+
+<div class="row">
+    <!-- left -->
+    <div class="col-3 pink-background">
+        <center>
+            @if (Route::has('profile'))
+            <a href="{{ route('profile') }}">
+                <img src="{{ url('displayImage/'.$image.'')}}" style="width: 120px; height: 120px; margin-top: 50px" alt="">
+            </a>
+            @endif
+            <h3 style="font-style: poppins; margin-top: 20px; font-size:120%"><b>{{$name}}</b></h3>
+            <h3 style="font-style: poppins, sans-serif; margin-top: 10px; font-size:100%">{{$username}}</h3>
+            <h3 style="font-style: poppins; margin-top: 10px; font-size:100%">{{$following}} Following | {{$follower}} Followers</h3>
+            @if(!$isFollowing)
+            <form method="POST" action="{{ route('profileView.follow') }}" >
+                @csrf
+                <input type="hidden" name="following_id" value="{{ $id }}">
+                <button type="submit" class="btn btn-primary ml-2" style="background-color: #FFFFFF; color: #FD7EC2; border-color:hotpink">Follow</button>
+            </form>
+            @else
+            <form method="POST" action="{{ route('profileView.unfollow') }}" >
+                @csrf
+                <input type="hidden" name="following_id" value="{{ $id }}" hidden>
+                <button type="submit" class="btn btn-primary ml-2" style="background-color: #FFFFFF; color: #FD7EC2; border-color:hotpink">Unfollow</button>
+            </form>
+            @endif
+            <!-- <h3 style="font-style: poppins; margin-top: 20px; font-size:100%">Selebgram | Singer</h3> -->
+        </center>
+    </div>
+
+    <!-- middle-->
+    <div class="col-6 middle">
+        <div class="hidden-card">
+        </div>
+
+        <div class="row">
+            <section class="container">
+                <ul class="nav nav-tabs pt-5">
+                    @if($isDiskusi)
+                    <li class="nav-item"><a class="nav-link active" href="{{ url('profileView/diskusi/'.$id) }}" id="btn-diskusi">Diskusi</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ url('profileView/event/'.$id) }}">Events</a></li>
+                    @else
+                    <li class="nav-item"><a class="nav-link" href="{{ url('profileView/diskusi/'.$id) }}" id="btn-diskusi">Diskusi</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="{{ url('profileView/event/'.$id) }}">Events</a></li>
+                    @endif
+                </ul>
+            </section>
+        </div>
+        <!-- middle bottom -->
+        <div class="white-background">
+            <!-- End Show Post Data-->
+
+            <!-- Start Show Post Data DISKUSI-->
+            @if($isDiskusi)
+            <ul class="panel-activity__list" id="tab-diskusi">
+                @foreach ($posts as $post)
+                <div class="row">
+                    <i class="activity__list__icon fa fa-question-circle-o"></i>
+                    <div class="activity__list__header">
+                        <img src="{{ url('displayImage/'.$post->users->images_id.'')}}" style="width: 30px; height: 30px; margin:2px" alt="">
+                        <a href="#" style="color: #FD7EC2; font-size:medium">{{$post->users->name }}</a>
+                        @if (Route::has('diskusi'))
+                        <br>
+                        <a href="{{ url('diskusi/'.$post->id) }}" style="color: black; font-size:large;">
+                            {{ $post->story }}
+                        </a>
+                        @endif
+                    </div>
+                    <div class="activity__list__body entry-content">
+                        <!-- <strong>Lorem ipsum dolor sit amet</strong>, consectetur adipisicing elit. Voluptatibus ab a nostrum repudiandae dolorem ut quaerat veniam asperiores, rerum voluptatem magni dolores corporis!
+            <em>Molestiae commodi nesciunt a, repudiandae repellendus ea.</em> -->
+                    </div>
+                    <br>
+                    <div class="container">
+                        <a href="#" style="color: #FD7EC2;"> <i class="fa fa-thumbs-up"></i>Like</a>
+                        <a href="#" style="color: #FD7EC2;"> <i class="fa fa-comments"></i>Comment</a>
+                        <div class="align-bottom ml-auto" style="float:right;">
+                            <span style="color:grey;"> <i class="fa fa-clock"></i>a minute ago</span>
+                        </div>
+                    </div>
+
+                </div>
+                @endforeach
+            </ul>
+            <!-- End Show Post Data-->
+            @else
+            <!-- Start Show Post Data EVENT-->
+            <ul class="panel-activity__list" id="tab-events">
+                <div class="row">
+                    @foreach ($events as $event)
+
+                    <form method="POST" action="{{ route('daftarEvent') }}">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <input type="hidden" name="events_id" value="{{ $event->id }}">
+
+                        <div class="row">
+                            <i class="activity__list__icon fa fa-question-circle-o"></i>
+                            <div class="activity__list__header container2-left">
+                                @if (Route::has('kolaborasi'))
+                                <br>
+                                <a href="{{ url('eventSaya/'.$event->id.'') }}" style="color: black; font-size:large;">
+                                    {{ $event->event_name }}
+                                </a>
+                            </div>
+                            <div class='container2-right'>
+                                <button type="submit" class="btn btn-primary ml-2" style="background-color: #FFFFFF; color: #FD7EC2; border-color:hotpink">Daftar</button>
+                            </div>
+                    </form>
+                    @endif
+
+                    <div>
+                        <div class="activity__list__body entry-content">
+                        </div>
+                        <br>
+                        <div class="align-bottom">
+                            <span style="font-family: Verdana, Geneva, Tahoma, sans-serif;">Tanggal:</span>
+                            <div class="ml-auto" style="float:right;">
+                                <span style="color:grey;"></i>dibuat oleh: {{$event->users[0]->name }}
+                                    <img src="{{ url('displayImage/'.$event->users[0]->images_id.'')}}" style="width: 30px; height: 30px; margin:2px" alt="">
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+
+            </ul>
+            @endif
+
+            <!-- End Show Post Data-->
+
+        </div>
+    </div>
+
+</div>
+</div>
+</div>
+@endsection
